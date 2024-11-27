aa2f574bcc2ba908060074069536bb748d9decb0
actboy168 <actboy168@gmail.com>
2024-11-27 19:58:59 +0800

    Update httpc.lua

--- callback/http/httpc.lua
+++ callback/http/httpc.lua
@@ -1,42 +1,61 @@
 local net = require "net"
 local internal = require "http.internal"
 
+local string = string
+local table = table
+local pcall = pcall
+local error = error
+local pairs = pairs
+
 local httpc = {}
 
 local function check_protocol(host)
-    local protocol = host:match("^%a+://")
+    local protocol = host:match("^[Hh][Tt][Tt][Pp][Ss]?://")
     if protocol then
+        host = string.gsub(host, "^" .. protocol, "")
         protocol = string.lower(protocol)
-        if protocol == "http://" then
-            return string.gsub(host, "^"..protocol, "")
+        if protocol == "https://" then
+            return "https", host
+        elseif protocol == "http://" then
+            return "http", host
         else
             error(string.format("Invalid protocol: %s", protocol))
         end
     else
-        return host
+        return "http", host
     end
 end
 
-local function gen_interface(fd)
+local function gen_interface(protocol, fd)
+    if protocol ~= "http" then
+        error(string.format("Invalid protocol: %s", protocol))
+    end
+
     local rbuf = ""
     function fd:on_data(data)
         rbuf = rbuf .. data
     end
+
     local function read(sz)
         if sz == nil then
-            net.update()
+            net.update(0)
             local r = rbuf
             rbuf = ""
             return r
         else
-            while not fd:is_closed() do
-                net.update()
+            if #rbuf >= sz then
+                local r = rbuf:sub(1, sz)
+                rbuf = rbuf:sub(sz + 1)
+                return r
+            end
+            repeat
                 if #rbuf >= sz then
                     local r = rbuf:sub(1, sz)
                     rbuf = rbuf:sub(sz + 1)
                     return r
                 end
-            end
+                net.update()
+            until fd:is_closed()
             return ""
         end
     end
@@ -52,29 +71,85 @@ local function gen_interface(fd)
         return r
     end
     return {
+        init = nil,
+        close = nil,
         read = read,
         write = write,
         readall = readall,
     }
 end
 
-function httpc.request(method, host, url, recvheader, header, content)
-    host = check_protocol(host)
-    local hostname, port = host:match"([^:]+):?(%d*)$"
-    port = port == "" and 80 or tonumber(port)
-    local fd = net.connect("tcp", hostname, port)
-    if not fd then
-        error(string.format("http connect error host:%s, port:%s, timeout:%s", hostname, port))
-        return
+local function connect(host)
+    local protocol
+    protocol, host = check_protocol(host)
+    local hostaddr, port = host:match "([^:]+):?(%d*)$"
+    if port == "" then
+        port = protocol == "http" and 80 or protocol == "https" and 443
+    else
+        port = tonumber(port)
     end
-	local interface = gen_interface(fd)
-    local ok , statuscode, body = pcall(internal.request, interface, method, host, url, recvheader, header, content)
+    local fd = net.connect("tcp", hostaddr, port)
+    if not fd then
+        error(string.format("%s connect error host:%s, port:%s", protocol, hostaddr, port))
+    end
+    local interface = gen_interface(protocol, fd)
+    if interface.init then
+        interface.init(host)
+    end
+    return fd, interface, host
+end
+
+local function close_interface(interface, fd)
+    interface.finish = true
     fd:close()
+    if interface.close then
+        interface.close()
+        interface.close = nil
+    end
+end
+
+function httpc.request(method, hostname, url, recvheader, header, content)
+    local fd, interface, host = connect(hostname)
+    local ok, statuscode, body, header = pcall(internal.request, interface, method, host, url, recvheader, header,
+        content)
+    if ok then
+        ok, body = pcall(internal.response, interface, statuscode, body, header)
+    end
+    close_interface(interface, fd)
     if ok then
         return statuscode, body
     else
+        error(body or statuscode)
+    end
+end
+
+function httpc.head(hostname, url, recvheader, header, content)
+    local fd, interface, host = connect(hostname)
+    local ok, statuscode = pcall(internal.request, interface, "HEAD", host, url, recvheader, header, content)
+    close_interface(interface, fd)
+    if ok then
+        return statuscode
+    else
         error(statuscode)
     end
+end
+
+function httpc.request_stream(method, hostname, url, recvheader, header, content)
+    local fd, interface, host = connect(hostname)
+    local ok, statuscode, body, header = pcall(internal.request, interface, method, host, url, recvheader, header,
+        content)
+    interface.finish = true -- don't shutdown fd in timeout
+    local function close_fd()
+        close_interface(interface, fd)
+    end
+    if not ok then
+        close_fd()
+        error(statuscode)
+    end
+    -- todo: stream support timeout
+    local stream = internal.response_stream(interface, statuscode, body, header)
+    stream._onclose = close_fd
+    return stream
 end
 
 function httpc.get(...)
@@ -92,10 +167,11 @@ function httpc.post(host, url, form, recvheader)
         ["content-type"] = "application/x-www-form-urlencoded"
     }
     local body = {}
-    for k,v in pairs(form) do
-        table.insert(body, string.format("%s=%s",escape(k),escape(v)))
+    for k, v in pairs(form) do
+        table.insert(body, string.format("%s=%s", escape(k), escape(v)))
     end
-    return httpc.request("POST", host, url, recvheader, header, table.concat(body , "&"))
+
+    return httpc.request("POST", host, url, recvheader, header, table.concat(body, "&"))
 end
 
 return httpc
